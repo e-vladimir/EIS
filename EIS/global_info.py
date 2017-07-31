680fdbc04f184018b69d8f1428e6924ecb5a9979
e-vladimir <79600069487@yandex.ru>
2017-07-31 12:53:14 +0300

    Отображение Index.html

---
+++ EIS/global_info.py
@@ -0,0 +1,2 @@
+EIS_NAME    = 'АПТ'
+EIS_VERSION = '0.17.07.31'
--- EIS/views.py
+++ EIS/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from EIS.global_info import *
 
 
 def page_index(request):
-	return render(request, 'index.html', {})
+	return render(request, 'index.html', {'EIS_NAME': EIS_NAME, 'EIS_VERSION': EIS_VERSION})
