a4f8e2e60158941612dbf573f75247cb070cd0b2
e-vladimir <79600069487@yandex.ru>
2017-08-01 14:14:04 +0300

    Внешний вид Контур "Документы"

--- documents/views.py
+++ documents/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib import auth
+from EIS.global_info import *
+from .models import EIS_Document
 
-# Create your views here.
+
+def page_documents(request):
+	if request.user.is_authenticated():
+		return render(request, 'documents.html', {'EIS_NAME': EIS_NAME, 'EIS_VERSION': EIS_VERSION})
+	else:
+		return render(request, 'index_public.html', {'EIS_NAME': EIS_NAME, 'EIS_VERSION': EIS_VERSION})
+
+
+def page_document_new(request):
+	pass
