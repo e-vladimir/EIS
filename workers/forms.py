d1d3da9e477e84755ed2b1c85e7f9266859192f9
e-vladimir <79600069487@yandex.ru>
2017-08-19 19:28:47 +0300

    Сотрудники + Фоновый текст

--- workers/forms.py
+++ workers/forms.py
@@ -7,3 +7,9 @@ class WorkerForm(forms.ModelForm):
 	class Meta:
 		model = EIS_Worker
 		fields = ['category', 'post', 'name', 'phone_mobile', 'phone', 'email', 'note']
+
+	def __init__(self, *args, **kwargs):
+		super(WorkerForm, self).__init__(*args, **kwargs)
+		self.fields['phone_mobile'].widget = forms.TextInput(attrs={'placeholder': '+7 (___) ___-__-__'})
+		self.fields['phone'].widget = forms.TextInput(attrs={'placeholder': '+7 (____) __-__-__'})
+		self.fields['email'].widget = forms.TextInput(attrs={'placeholder': '<адрес>@<домен>.ru'})
