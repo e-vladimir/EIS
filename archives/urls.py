53eba6476c0f1d724dc78f7936d68ec066fb7dd8
e-vladimir <79600069487@yandex.ru>
2018-07-14 13:21:43 +0300

    Архив: * Корректировка обработки URL

--- archives/urls.py
+++ archives/urls.py
@@ -2,20 +2,12 @@ from django.urls import path
 from EIS import settings
 from . import views
 
-# /archives - список документов
-# /archives/period/<period> - список документов за указанный период
-# /archives/category/<category> - список документов в категории
-# /archives/edit/<num> - редактирование
-# /archives/view/<num> - просмотр
-# /archives/new - создание
 
 urlpatterns = [
 	path('',                                              views.page_archives),
 
 	path('filter/<int:year>/',                            views.page_archives_filter),
-	path('filter/<int:year>/<str:category>',              views.page_archives_filter),
 	path('filter/<int:year>/<int:month>/',                views.page_archives_filter),
-	path('filter/<int:year>/<int:month>/<str:category>/', views.page_archives_filter),
 
 	path('filter/<str:category>/',                        views.page_archives_filter),
 	path('filter/<str:category>/<int:year>/',             views.page_archives_filter),
